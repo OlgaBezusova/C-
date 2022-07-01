7a1064e82d1a409b542b4239f0b465b97f8b3705
OlgaBezusova <bezusova22@yandex.ru>
2022-07-01 09:05:15 +0300

    test6

---
+++ test6/Program.cs
@@ -0,0 +1,14 @@
+﻿/* напишите программу, которая выводит случайное число из отрезка  [10,99] и показывает наибщльщую цифру числа
+78 - > 8
+12 - > 2
+85 - > 8
+*/
+
+int GetRandomNumber(int minBorder, int maxBorder);
+{
+    int result = new Random().Next(minBorder, maxBorder+1 );
+    return result;
+}
+int randomNumber = GetRandomNumber(10,99);
+Console.WriteLine(randomNumber);
+
