1d3160dbe080c0d7bca87ad527ce329ecb0319eb
OlgaBezusova <bezusova22@yandex.ru>
2022-07-03 11:04:07 +0300

    два числа, проверка на квадрат

---
+++ test10/Program.cs
@@ -0,0 +1,13 @@
+﻿/* программа, принимает два числа, проверяет является второе число квадротом первого */
+
+
+int a, b;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b){
+Console.WriteLine("yes");
+}
+else{
+Console.WriteLine("no");
+}
+
