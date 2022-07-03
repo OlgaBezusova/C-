6a205789176e569385fa0d5df5b6999019bd4ef6
OlgaBezusova <bezusova22@yandex.ru>
2022-07-03 10:52:42 +0300

    число кратное 7 и 23

---
+++ test9/Program.cs
@@ -0,0 +1,14 @@
+﻿/* программа, на вход принимает число, проверяет его кратно ли оно одновременно
+ 7 и 23.*/
+
+ int n;
+ n=Convert.ToInt32(Console.ReadLine());
+ if (n%7==0 && n%23==0) 
+ {
+    Console.WriteLine("yes");
+ }
+ else
+ {
+    Console.WriteLine("no");
+ }
+ 
