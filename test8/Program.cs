df04cab9be41a7ebe57f912079a58b27e82843e8
OlgaBezusova <bezusova22@yandex.ru>
2022-07-03 10:21:00 +0300

    кратное второе число первому

---
+++ test8/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+34, 5 -> не кратно, остаток 4
+16, 4 -> кратно */
+
+int num_1 = Convert.ToInt32(Console.ReadLine());
+int num_2 = Convert.ToInt32(Console.ReadLine());
+
+if (num_1 % num_2 == 0)
+{ 
+    Console.WriteLine("yes");
+}
+else 
+Console.WriteLine(num_1%num_2);
