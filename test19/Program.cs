75838c198ec271626ba7aa10247601d948a05157
OlgaBezusova <bezusova22@yandex.ru>
2022-07-06 09:13:20 +0300

    расстояние мужду точками

---
+++ test19/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 21: Напишите программу, которая принимает
+ на вход координаты двух точек и находит расстояние между ними в 2D пространстве. */
+
+Console.WriteLine("введите координаты точки А: x= ");
+ double Ax=Convert.ToDouble(Console.ReadLine());
+ Console.WriteLine("y= ");
+ double Ay=Convert.ToDouble(Console.ReadLine());
+ Console.WriteLine("введите координаты точки B: x= ");
+ double Bx=Convert.ToDouble(Console.ReadLine());
+ Console.WriteLine("y= ");
+ double By=Convert.ToDouble(Console.ReadLine());
+
+ double distance=Math.Sqrt(Math.Pow((Ax-Bx),2)+Math.Pow((Ay-By),2));
+ Console.WriteLine(distance);
+ 
+
+ 
+
