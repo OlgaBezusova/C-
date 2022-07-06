0653dd8e244be58a9c260d6e1616f0d39a9b8be4
OlgaBezusova <bezusova22@yandex.ru>
2022-07-06 11:30:16 +0300

    куб чисел до N

---
+++ Test23_dz/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу,
+которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+
+Console.Write("Введите число N  ");
+int N=Convert.ToInt32(Console.ReadLine());
+int i;
+double cube;
+
+for (i=1; i<N; i++)
+{
+    cube=Math.Pow(i,3);
+    Console.WriteLine($"{cube}, ");
+} 
+cube=Math.Pow(i,3);
+Console.WriteLine(cube);
