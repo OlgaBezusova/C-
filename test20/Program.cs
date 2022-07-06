ebd0ea72fb6215b3ec23d7be64d750d00c1c7f96
OlgaBezusova <bezusova22@yandex.ru>
2022-07-06 09:45:12 +0300

    последовательность N квадрат

---
+++ test20/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4 */
+
+Console.Write("Введите число N  ");
+int N=Convert.ToInt32(Console.ReadLine());
+int i;
+double square;
+
+for (i=1; i<N; i++)
+{
+    square=Math.Pow(i,2);
+    Console.WriteLine($"{square}, ");
+} 
+square=Math.Pow(i,2);
+Console.WriteLine(square);
