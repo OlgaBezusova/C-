ece36188135e31995c958a97b8c94240e758fbee
OlgaBezusova <bezusova22@yandex.ru>
2022-07-09 16:12:40 +0300

    количество цифр в числе

---
+++ test26/Program.cs
@@ -0,0 +1,11 @@
+﻿/* программа, принимает на вход число и 
+выдает колличество цифр в числе.*/
+
+int n=Convert.ToInt32(Console.ReadLine());
+int count=0;
+while (n > 0)
+{
+    count=count+1;
+    n=n/10;
+}
+Console.WriteLine(count);
