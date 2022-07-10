2ad16af1f38ba0fad1e8d732dcff50144c67dea4
OlgaBezusova <bezusova22@yandex.ru>
2022-07-10 15:29:06 +0300

    произведение чисел до N

---
+++ test28/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 28: Напишите программу, которая принимает на вход число N 
+и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120*/
+
+int factorial(int number)
+{
+    int result = 1;
+    for (int i=1; i<=number; i++)
+    {
+        result = result*i;
+    }
+    return result;
+}
+
+Console.Write("Введите число N  ");
+int number=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"факториал числа{number} равен {factorial(number)}");
