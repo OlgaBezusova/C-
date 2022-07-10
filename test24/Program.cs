c14d41cf493de917f8e2612c1520caf91fa801d2
OlgaBezusova <bezusova22@yandex.ru>
2022-07-10 15:03:48 +0300

    сумма до числа А

--- test24/Program.cs
+++ test24/Program.cs
@@ -1,10 +1,26 @@
-﻿Console.WriteLine("Введите число A: ");
+﻿/*Console.WriteLine("Введите число A: ");
 int sum = 0, n = Convert.ToInt32(Console.ReadLine());
 for (int i = 1; i <= n; i++)
 {
 sum = sum + i;
 }
-Console.WriteLine(sum);
+Console.WriteLine(sum); */
 
 
 
+int GetSumOfRange(int begnumber=1, int endnumber=1)
+{
+    var sumOfrange=0;
+    for (int i = begnumber; i <= endnumber; i++) 
+    {
+        sumOfrange+=i;
+    }
+    return sumOfrange;
+
+}
+Console.WriteLine("Введите число А для получения суммы : ");
+
+int endnumberForUser=Convert.ToInt32(Console.ReadLine());
+int sumOfRange=GetSumOfRange(endnumber : endnumberForUser);
+
+Console.WriteLine($"{endnumberForUser}->{sumOfRange}");
