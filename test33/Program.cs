1729b2fa2fe564f81a9a041b7a0e3d9e0e894f87
OlgaBezusova <bezusova22@yandex.ru>
2022-07-12 20:23:57 +0300

    проверка числа в массиве

---
+++ test33/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine($"Введите число :");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[6];
+ for (int i = 0; i < 6; i++)
+    {
+        array[i] = new Random().Next(0,101);
+    }
+Console.WriteLine("["+ string.Join("," , array) + "]");
+  bool log = false;
+for (int i = 0; i < 6; i++)
+{
+    if (n==array[i]) 
+    {
+        log=true;
+        break;
+    }
+
+}
+if (log)
+{
+    Console.WriteLine("Число присутствует в массиве");
+}
+else
+{
+    Console.WriteLine("Числа нет в массиве");
+}
+
+
+
+
+
+
