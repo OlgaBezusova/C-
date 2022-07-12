c0365d236c53413f08c7ca5f49b3786c25bfddc4
OlgaBezusova <bezusova22@yandex.ru>
2022-07-12 09:13:56 +0300

    массив из 8 элементов

--- test30/Program.cs
+++ test30/Program.cs
@@ -1,30 +1,38 @@
-﻿/* программа, к-я выводит массив из 8 элементов, 
-заполненный 0 и 1 в случайном порядке.*/
+﻿/* программа, к-я выводит массив из 8 элементов.*/
 
 int[] array = new int[8];
 void FillArray(int[] collection)
 {
-int length = collection.Length;
-int index = 0;
-while (index < length)
-{
-collection[index] = new Random().Next(0, 2);
-index++;
-}
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0,99);
+        index++;
+    }
 }
 
 void PrintArray(int[] col)
 {
-int count = col.Length;
-int position = 0;
-while (position < count)
-{
-Console.WriteLine(col[position]);
-position++;
-}
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
 }
 
 FillArray(array);
 PrintArray(array);
+
+/* int[] array = new int[8];
+for (int i = 0; i <= array.Length; i++)
+{
+    array[i] = new Random().Next(0, 2);
+    Console.WriteLine(array[i]);
+} */
+
+
 
 
