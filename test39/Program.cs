4df5d43f21784ca72b928f6663643c287bb659c7
OlgaBezusova <bezusova22@yandex.ru>
2022-07-19 23:01:22 +0300

    переворот массива

--- test39/Program.cs
+++ test39/Program.cs
@@ -4,42 +4,26 @@
 [1 2 3 4 5] -> [5 4 3 2 1]
 [6 7 3 6] -> [6 3 7 6]*/ 
 
-int GenerateArray(int lenght, int deviation)
+int[] f(int[] array)
 {
-    int[] result= new int[length];
-    for (var i = 0; i < length; i++)
+    for(int i = 0; i < array.Length/2; i++)
     {
-        result[i]=new Random().Next(-deviation, deviation+1);
-    } 
-    return result;
-}
-void printArray(int [] arrayToPrint)
-{
-    Console.Write("[");
-    for (var i = 0; i <  arrayToPrint.length; i++)
-    {
-         Console.Write(arrayToPrint[i]);
-         if (i<arrayToPrint.Length-1)
-         {
-            Console.Write(", ");
-         }
+        (array[i], array[array.Length-(i+1)]) = (array[array.Length-(i+1)], array[i]);
     }
-    Console.Write("]");
+    return array;
 }
-int[] reverseArray(int[] arrayToReverse);
+int n;
+Console.WriteLine("Задайте длину массива : ");
+n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+var random = new Random();
+for(int i = 0; i < array.Length; i++)
 {
-    int[] reversedArray=new int[arrayToReverse.Length];
-    for (int i = 0; i < arrayToReverse.Length; i++)
-    {
-        reversedArray[i]=arrayToReverse[arrayToReverse.Length-i-1];
-    } 
-    return reversedArray;
+    array[i] = random.Next(-99,99);
 }
-
-int[] sourceArray=generateArray(10,20);
-int[] reversedArray=reverseArray(sourceArray);
-printArray(sourceArray);
-printArray(reversedArray);
+Console.WriteLine("Наш массив: ");
+Console.WriteLine("[" + string.Join(",", array) + "]");
+Console.WriteLine("[" + string.Join(",", f(array)) + "]");
 
 
 
