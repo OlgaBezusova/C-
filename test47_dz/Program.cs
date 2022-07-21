c0b8d51aeab32cc17c713f6b473e86f952876d07
OlgaBezusova <bezusova22@yandex.ru>
2022-07-21 13:19:41 +0300

    задача 47

--- test47_dz/Program.cs
+++ test47_dz/Program.cs
@@ -6,14 +6,14 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9*/
 
-double[, ] CreateMatr(int n, int m)
+double[, ] CreateMatr(int n, int m )
 {
     double[, ] matrix = new double[n ,m];
     for (int i = 0; i < matrix.GetLength(0); i++)
         {
             for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                matrix[i, j] = (new Random().NextDouble())*10;
+                matrix[i, j] =new Random().NextDouble()*10;
             }
         }
     return matrix;
@@ -26,12 +26,15 @@ void PrintMatr(double[, ] matrix)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                Console.Write(matrix[i, j] + " ");
-            }
+                Console.Write(Math.Round(matrix[i, j],2)+ " ");
+            }   
         Console.WriteLine();
+        
     }
 }
-
+Console.Write($"Введи число n: ");
 int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введи число m: ");
 int m = Convert.ToInt32(Console.ReadLine());
 PrintMatr(CreateMatr(n, m));
+
