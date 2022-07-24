2c06d882f4b975250d580e4536b6d7a990053c05
OlgaBezusova <bezusova22@yandex.ru>
2022-07-24 14:17:49 +0300

    строка с наименьшей суммой элементов

---
+++ test56_dz/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задача 56: Задайте прямоугольный двумерный массив.
+ Напишите программу, которая будет находить строку 
+ с наименьшей суммой элементов.*/
+
+int[, ] CreateMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int x = new Random().Next(1, 100);
+            matrix[i , j] = x;
+        }
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+
+    return matrix;
+}
+
+
+void NewMatrix(int[, ] matrix)
+{
+    Console.WriteLine();
+    int minrow = 1000;
+    int kmin = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sumrow = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumrow = sumrow + matrix[i, j];
+        }
+        if (sumrow < minrow)
+        {
+            minrow = sumrow;
+            kmin = i + 1;
+        }
+    }
+    Console.WriteLine("сумма " + minrow + "  строка " + kmin);
+
+}
+
+int[, ] matrix = new int[3, 3];
+matrix = CreateMatrix(matrix);
+NewMatrix(matrix);
