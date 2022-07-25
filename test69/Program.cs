9b729035dede8cb5215dc5cc1b84e1f6bb554ff9
OlgaBezusova <bezusova22@yandex.ru>
2022-07-26 01:09:01 +0300

    на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии

---
+++ test69/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 69: Напишите программу, 
+которая на вход принимает два числа A и B, 
+и возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8 */
+
+int Power(int a, int b)
+{
+return b==0? 1:Power(a, b-1)*a;
+
+}
+
+Console.WriteLine("Введите число А: ");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Power(a,b));
+
+
+
