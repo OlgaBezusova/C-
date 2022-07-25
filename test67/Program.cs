024476ba668053f56950ebe2d5fe88a549adf2b7
OlgaBezusova <bezusova22@yandex.ru>
2022-07-26 00:56:17 +0300

    принимать на вход число и возвращать сумму его цифр

---
+++ test67/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9 */
+
+int DigitSum(int num=1)
+{
+    if (num/10!=0) 
+    {
+        return num%10+DigitSum(num/10);
+    }
+    else
+    {
+        return num;
+    }
+}
+
+Console.Write("Введите число: ");
+int num=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(DigitSum(num));
+
+
+
+
+
